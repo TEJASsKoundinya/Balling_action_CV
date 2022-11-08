9705545eb29405bde65b91f95fda9efb7d7165d6
Tejas S Koundinya <77600063+TEJASsKoundinya@users.noreply.github.com>
2022-11-08 23:57:12 +0530

    Add files via upload

---
+++ DataGen.py
@@ -0,0 +1,359 @@
+import cv2 as cv
+import numpy as np
+from keras.models import load_model
+from PIL import Image
+import tensorflow as tf 
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
+import matplotlib.patches as patches
+
+import imageio
+from IPython.display import HTML, display
+
+module = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/4")
+input_size = 192
+def getOutputsNames(net):
+    # Get the names of all the layers in the network
+    layersNames = net.getLayerNames()
+    # Get the names of the output layers, i.e. the layers with unconnected outputs
+    return [layersNames[i - 1] for i in net.getUnconnectedOutLayers()]
+
+KEYPOINT_DICT = {
+    'nose': 0,
+    'left_eye': 1,
+    'right_eye': 2,
+    'left_ear': 3,
+    'right_ear': 4,
+    'left_shoulder': 5,
+    'right_shoulder': 6,
+    'left_elbow': 7,
+    'right_elbow': 8,
+    'left_wrist': 9,
+    'right_wrist': 10,
+    'left_hip': 11,
+    'right_hip': 12,
+    'left_knee': 13,
+    'right_knee': 14,
+    'left_ankle': 15,
+    'right_ankle': 16
+}
+
+# Maps bones to a matplotlib color name.
+KEYPOINT_EDGE_INDS_TO_COLOR = {
+    (0, 1): 'm',
+    (0, 2): 'c',
+    (1, 3): 'm',
+    (2, 4): 'c',
+    (0, 5): 'm',
+    (0, 6): 'c',
+    (5, 7): 'm',
+    (7, 9): 'm',
+    (6, 8): 'c',
+    (8, 10): 'c',
+    (5, 6): 'y',
+    (5, 11): 'm',
+    (6, 12): 'c',
+    (11, 12): 'y',
+    (11, 13): 'm',
+    (13, 15): 'm',
+    (12, 14): 'c',
+    (14, 16): 'c'
+}
+
+def _keypoints_and_edges_for_display(keypoints_with_scores,
+                                     height,
+                                     width,
+                                     keypoint_threshold=0.11):
+  """Returns high confidence keypoints and edges for visualization.
+
+  Args:
+    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
+      the keypoint coordinates and scores returned from the MoveNet model.
+    height: height of the image in pixels.
+    width: width of the image in pixels.
+    keypoint_threshold: minimum confidence score for a keypoint to be
+      visualized.
+
+  Returns:
+    A (keypoints_xy, edges_xy, edge_colors) containing:
+      * the coordinates of all keypoints of all detected entities;
+      * the coordinates of all skeleton edges of all detected entities;
+      * the colors in which the edges should be plotted.
+  """
+  keypoints_all = []
+  keypoint_edges_all = []
+  edge_colors = []
+  num_instances, _, _, _ = keypoints_with_scores.shape
+  for idx in range(num_instances):
+    kpts_x = keypoints_with_scores[0, idx, :, 1]
+    kpts_y = keypoints_with_scores[0, idx, :, 0]
+    kpts_scores = keypoints_with_scores[0, idx, :, 2]
+    kpts_absolute_xy = np.stack(
+        [width * np.array(kpts_x), height * np.array(kpts_y)], axis=-1)
+    kpts_above_thresh_absolute = kpts_absolute_xy[
+        kpts_scores > keypoint_threshold, :]
+    keypoints_all.append(kpts_above_thresh_absolute)
+
+    for edge_pair, color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
+      if (kpts_scores[edge_pair[0]] > keypoint_threshold and
+          kpts_scores[edge_pair[1]] > keypoint_threshold):
+        x_start = kpts_absolute_xy[edge_pair[0], 0]
+        y_start = kpts_absolute_xy[edge_pair[0], 1]
+        x_end = kpts_absolute_xy[edge_pair[1], 0]
+        y_end = kpts_absolute_xy[edge_pair[1], 1]
+        line_seg = np.array([[x_start, y_start], [x_end, y_end]])
+        keypoint_edges_all.append(line_seg)
+        edge_colors.append(color)
+  if keypoints_all:
+    keypoints_xy = np.concatenate(keypoints_all, axis=0)
+  else:
+    keypoints_xy = np.zeros((0, 17, 2))
+
+  if keypoint_edges_all:
+    edges_xy = np.stack(keypoint_edges_all, axis=0)
+  else:
+    edges_xy = np.zeros((0, 2, 2))
+  return keypoints_xy, edges_xy, edge_colors
+
+
+def draw_prediction_on_image(
+    image, keypoints_with_scores, crop_region=None, close_figure=False,
+    output_image_height=None):
+  """Draws the keypoint predictions on image.
+
+  Args:
+    image: A numpy array with shape [height, width, channel] representing the
+      pixel values of the input image.
+    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
+      the keypoint coordinates and scores returned from the MoveNet model.
+    crop_region: A dictionary that defines the coordinates of the bounding box
+      of the crop region in normalized coordinates (see the init_crop_region
+      function below for more detail). If provided, this function will also
+      draw the bounding box on the image.
+    output_image_height: An integer indicating the height of the output image.
+      Note that the image aspect ratio will be the same as the input image.
+
+  Returns:
+    A numpy array with shape [out_height, out_width, channel] representing the
+    image overlaid with keypoint predictions.
+  """
+  height, width, channel = image.shape
+  aspect_ratio = float(width) / height
+  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
+  # To remove the huge white borders
+  fig.tight_layout(pad=0)
+  ax.margins(0)
+  ax.set_yticklabels([])
+  ax.set_xticklabels([])
+  plt.axis('off')
+
+  im = ax.imshow(image)
+  line_segments = LineCollection([], linewidths=(4), linestyle='solid')
+  ax.add_collection(line_segments)
+  # Turn off tick labels
+  scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
+
+  (keypoint_locs, keypoint_edges,
+   edge_colors) = _keypoints_and_edges_for_display(
+       keypoints_with_scores, height, width)
+
+  line_segments.set_segments(keypoint_edges)
+  line_segments.set_color(edge_colors)
+  if keypoint_edges.shape[0]:
+    line_segments.set_segments(keypoint_edges)
+    line_segments.set_color(edge_colors)
+  if keypoint_locs.shape[0]:
+    scat.set_offsets(keypoint_locs)
+
+  if crop_region is not None:
+    xmin = max(crop_region['x_min'] * width, 0.0)
+    ymin = max(crop_region['y_min'] * height, 0.0)
+    rec_width = min(crop_region['x_max'], 0.99) * width - xmin
+    rec_height = min(crop_region['y_max'], 0.99) * height - ymin
+    rect = patches.Rectangle(
+        (xmin,ymin),rec_width,rec_height,
+        linewidth=1,edgecolor='b',facecolor='none')
+    ax.add_patch(rect)
+
+  fig.canvas.draw()
+  image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+  image_from_plot = image_from_plot.reshape(
+      fig.canvas.get_width_height()[::-1] + (3,))
+  plt.close(fig)
+  if output_image_height is not None:
+    output_image_width = int(output_image_height / height * width)
+    image_from_plot = cv2.resize(
+        image_from_plot, dsize=(output_image_width, output_image_height),
+         interpolation=cv2.INTER_CUBIC)
+  return image_from_plot
+
+def to_gif(images, fps):
+  """Converts image sequence (4D numpy array) to gif."""
+  imageio.mimsave('./animation.gif', images, fps=fps)
+  return embed.embed_file('./animation.gif')
+
+def progress(value, max=100):
+  return HTML("""
+      <progress
+          value='{value}'
+          max='{max}',
+          style='width: 100%'
+      >
+          {value}
+      </progress>
+  """.format(value=value, max=max))
+
+def draw_prediction_on_image(
+        image, keypoints_with_scores, crop_region=None, close_figure=False,
+        output_image_height=None):
+
+  height, width, channel = image.shape
+  aspect_ratio = float(width) / height
+  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
+  # To remove the huge white borders
+  fig.tight_layout(pad=0)
+  ax.margins(0)
+  ax.set_yticklabels([])
+  ax.set_xticklabels([])
+  plt.axis('off')
+
+  im = ax.imshow(image)
+  line_segments = LineCollection([], linewidths=(4), linestyle='solid')
+  ax.add_collection(line_segments)
+  # Turn off tick labels
+  scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
+
+  (keypoint_locs, keypoint_edges,
+   edge_colors) = _keypoints_and_edges_for_display(
+       keypoints_with_scores, height, width)
+
+  line_segments.set_segments(keypoint_edges)
+  line_segments.set_color(edge_colors)
+  if keypoint_edges.shape[0]:
+    line_segments.set_segments(keypoint_edges)
+    line_segments.set_color(edge_colors)
+  if keypoint_locs.shape[0]:
+    scat.set_offsets(keypoint_locs)
+
+  if crop_region is not None:
+    xmin = max(crop_region['x_min'] * width, 0.0)
+    ymin = max(crop_region['y_min'] * height, 0.0)
+    rec_width = min(crop_region['x_max'], 0.99) * width - xmin
+    rec_height = min(crop_region['y_max'], 0.99) * height - ymin
+    rect = patches.Rectangle(
+        (xmin, ymin), rec_width, rec_height,
+        linewidth=1, edgecolor='b', facecolor='none')
+    ax.add_patch(rect)
+
+  fig.canvas.draw()
+  image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+  image_from_plot = image_from_plot.reshape(
+      fig.canvas.get_width_height()[::-1] + (3,))
+  plt.close(fig)
+  if output_image_height is not None:
+    output_image_width = int(output_image_height / height * width)
+    image_from_plot = cv2.resize(
+        image_from_plot, dsize=(output_image_width, output_image_height),
+        interpolation=cv2.INTER_CUBIC)
+  return image_from_plot
+
+vidcap = cv.VideoCapture("/home/user/TEJAS/Kreeda/videoplayback.mp4")
+s,img = vidcap.read()
+c = 0
+s = True
+
+while s:
+  img = vidcap.read()
+  if cv.waitKey(1) == 27:
+        vidcap.release()
+        cv.destroyAllWindows()
+        break
+
+        # get frame from the video
+  s, img = vidcap.read()
+
+        # Stop the program if reached end of video
+  if not s:
+      break
+  img = AutoCrop(img)
+  black_img = np.zeros(img.shape)
+  confThreshold = 0.85  # Confidence threshold
+  nmsThreshold = 0.4  # Non-maximum suppression threshold
+  inpWidth = 416  # Width of network's input image
+  inpHeight = 416  # Height of network's input image
+    # Load names of classes
+  classesFile = r"COCO.names"
+  classes = None
+  with open(classesFile, 'rt') as f:
+        classes = f.read().rstrip('\n').split('\n')
+    # Give the configuration and weight files for the model and load the network using them.
+  modelConfiguration = r"yolov3.cfg"
+  modelWeights = r"yolov3.weights"
+  net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+  net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+  net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+  blob = cv.dnn.blobFromImage(img, 1 / 255, (inpWidth, inpHeight), [0, 0, 0], 1, crop=False)
+  net.setInput(blob)
+  outs = net.forward(getOutputsNames(net))
+  frameHeight = img.shape[0]
+  frameWidth = img.shape[1]
+
+  classIds = []
+  confidences = []
+  boxes = []
+  for out in outs:
+      for detection in out:
+            scores = detection[5:]
+            classId = np.argmax(scores)
+            confidence = scores[classId]
+            if confidence > confThreshold and classes[classId] == "person":
+                # if classes[classId] == "person":
+                center_x = int(detection[0] * frameWidth)
+                center_y = int(detection[1] * frameHeight)
+                width = int(detection[2] * frameWidth)
+                height = int(detection[3] * frameHeight)
+                left = int(center_x - width / 2)
+                top = int(center_y - height / 2)
+                classIds.append(classId)
+                confidences.append(float(confidence))
+                boxes.append([left, top, width, height])
+
+    # Perform non maximum suppression to eliminate redundant overlapping boxes with
+    # lower confidences.
+  indices = cv.dnn.NMSBoxes(boxes, confidences, confThreshold, nmsThreshold)
+  ROI_number = 100*c
+  for i in indices:
+        i = i
+        box = boxes[i]
+        left = box[0]
+        top = box[1]
+        width = box[2]
+        height = box[3]
+        cv.rectangle(img,(left,top),(left+width,top+height),(0,0,0),2)
+        ROI = img[top:top+height, left:left+width]
+
+        img1 = ROI.copy()
+        black_img = np.zeros(img1.shape)
+
+        input_image = tf.expand_dims(img1, axis=0)
+        input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
+
+        # Run model inference.
+        keypoints_with_scores = movenet(input_image)
+
+        # Visualize the predictions with image.
+        display_image = tf.expand_dims(black_img, axis=0)
+        display_image = tf.cast(tf.image.resize_with_pad(
+        display_image, 1280, 1280), dtype=tf.int32)
+        output_overlay = draw_prediction_on_image(
+        np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+
+        cv.imwrite('/home/user/TEJAS/Kreeda/DB/CricketIMG/keeper/out_{}.png'.format(ROI_number), output_overlay)
+        ROI_number += 1
+
+  print("check point --->>",c)
+  if cv.waitKey(10) == 27:
+    break
+  c +=1  
